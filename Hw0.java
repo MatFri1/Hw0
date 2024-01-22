569d825a10a5e089ad48ea4b4400da2efdfd4396
MatFri1 <matthew.frizzle@maine.edu>
2024-01-22 11:44:11 -0500

    Update Hw0.java

--- Hw0.java
+++ Hw0.java
@@ -1,3 +1,5 @@
 public class Hw0{
 public static void main(String[] args){
-    System.out.println("Hello World! Matt Frizzle");}}
+    System.out.println("Hello World!");
+    System.out.println("Matt Frizzle");}
+}
